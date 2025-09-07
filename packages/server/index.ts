c21e7b906092168add7cd8201f817c5cc443ccab
Arash <avafapour@innovqube.com>
2025-09-07 15:37:25 +0200

    connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
-
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello from the API!'
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
